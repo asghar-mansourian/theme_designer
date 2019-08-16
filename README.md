ef7aa75cbfb26fb0ca37f134784e2e9451415f21
alicompu <2014najafi@gmail.com>
2019-08-16 08:53:06 +0430

    Solved auth user
    Signed-off-by: alicompu <2014najafi@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-theme designer 
+These parts solved
+
+1. auth user
+2. part admin in controller
+3. part routes in web.php
+   3.part admin in views
+
+How to usage this app
+
+1. composer install
+2. create database
+3. php artisan migrate
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,14 @@
 <?php
 
-Route::view('/', 'admin.dashboard');
+/**
+ * Part Login
+ */
+Route::auth();
+
+/**
+ * Part Admin
+ */
+Route::prefix('admin')->namespace('Admin')->name('admin.')->middleware(['auth.admin'])->group(function () {
+
+    Route::get('dashboard', 'DashboardController@dashboard')->name('dashboard');
+});
