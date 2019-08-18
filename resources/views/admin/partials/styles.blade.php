1bea76ea19372f5a16fd40b94127a4bb5912ed63
Asghar Mansourian <mansourian2018@gmail.com>
2019-08-18 20:11:59 +0430

    add font face file

--- resources/views/admin/partials/styles.blade.php
+++ resources/views/admin/partials/styles.blade.php
@@ -3,3 +3,15 @@
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
+
+  <style>
+      @font-face {
+          font-family: 'BYekan';
+          src:url('{{url('../fonts/Byekan/BYekan.eot')}}');
+      url('{{url('../fonts/Byekan/BYekan.ttf')}}');
+      url({{url('../fonts/Byekan/BYekan.woff')}});
+      }
+      html{
+          font-family: BYekan !important ;
+      }
+  </style>
